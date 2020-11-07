37a002f13c5ba7f4a86ffb5b1a1bf8cafd5cf2f9
judge2010 <74088083+judge2010@users.noreply.github.com>
2020-11-07 15:53:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Heading="My console project";
+console.log(Heading);
+var firstname="Jujhar ";
+console.log(firstname);
+var secondname="Singh";
+console.log(secondname);
+var fullname=firstname.concat(secondname);
+console.log(fullname);
